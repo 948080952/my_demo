4e1e69e9b5b59176cf2ea7b0e648ab41103d1301
daipei <948080952@qq.com>
2016-03-02 20:15:02 +0800

    add StickerSample

---
+++ StickerSample/StickerSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StickerSample
+//
+//  Created by Dai Pei on 16/3/2.
+//  Copyright © 2016年 Dai Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
