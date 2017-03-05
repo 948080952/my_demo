ca646e6b1a56b60cf5b4d2537ece22d97334f949
DaiPei <daipei@hust.edu.cn>
2017-03-05 12:57:09 +0800

    finish the jelly jump partly

---
+++ JellyJump/JellyJump/UIView+JellyJump.m
@@ -0,0 +1,82 @@
+//
+//  UIView+JellyJump.m
+//  JellyJump
+//
+//  Created by DaiPei on 2017/3/5.
+//  Copyright © 2017年 DaiPei. All rights reserved.
+//
+
+#import "UIView+JellyJump.h"
+
+@implementation UIView (JellyJump)
+
+- (void)startJump {
+    
+    CGFloat sx = 1;
+    CGFloat sy = 1;
+    CGFloat sz = 1;
+    
+    CGFloat tx = 0;
+    CGFloat ty = 0;
+    CGFloat tz = 0;
+    
+    
+    CAKeyframeAnimation *jellyJump = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    
+    NSValue *value0 = [NSValue valueWithCATransform3D:CATransform3DMakeScale(sx, sy, sz)];
+    
+    sx = 1.1;
+    sy = 0.9;
+    ty = (1 - sy) * CGRectGetHeight(self.layer.bounds) / 2;
+    CATransform3D scale1 = CATransform3DMakeScale(sx, sy, sz);
+    CATransform3D move1 = CATransform3DMakeTranslation(tx, ty, tz);
+    NSValue *value1 = [NSValue valueWithCATransform3D:CATransform3DConcat(scale1, move1)];
+    
+    sx = 0.9;
+    sy = 1;
+    ty = -30;
+    CATransform3D scale2 = CATransform3DMakeScale(sx, sy, sz);
+    CATransform3D move2 = CATransform3DMakeTranslation(tx, ty, tz);
+    NSValue *value2 = [NSValue valueWithCATransform3D:CATransform3DConcat(scale2, move2)];
+    
+    sx = 1;
+    sy = 1;
+    ty = 0;
+    CATransform3D scale3 = CATransform3DMakeScale(sx, sy, sz);
+    CATransform3D move3 = CATransform3DMakeTranslation(tx, ty, tz);
+    NSValue *value3 = [NSValue valueWithCATransform3D:CATransform3DConcat(scale3, move3)];
+    
+    sx = 1.05;
+    sy = 0.92;
+    ty = (1 - sy) * CGRectGetHeight(self.layer.bounds) / 2;
+    CATransform3D scale4 = CATransform3DMakeScale(sx, sy, sz);
+    CATransform3D move4 = CATransform3DMakeTranslation(tx, ty, tz);
+    NSValue *value4 = [NSValue valueWithCATransform3D:CATransform3DConcat(scale4, move4)];
+    
+    sx = 1;
+    sy = 1;
+    ty = 0;
+    CATransform3D scale5 = CATransform3DMakeScale(sx, sy, sz);
+    CATransform3D move5 = CATransform3DMakeTranslation(tx, ty, tz);
+    NSValue *value5 = [NSValue valueWithCATransform3D:CATransform3DConcat(scale5, move5)];
+    
+    CATransform3D scale6 = CATransform3DMakeScale(sx, sy, sz);
+    CATransform3D move6 = CATransform3DMakeTranslation(tx, ty, tz);
+    NSValue *value6 = [NSValue valueWithCATransform3D:CATransform3DConcat(scale6, move6)];
+    
+    
+    
+    jellyJump.values = @[value0, value1, value2, value3, value4, value5, value6];
+    jellyJump.keyTimes = @[@(0), @(0.1), @(0.3), @(0.4), @(0.45), @(0.5), @(1)];
+    
+    jellyJump.duration = 1.5;
+    jellyJump.fillMode = kCAFillModeBackwards;
+    jellyJump.removedOnCompletion = NO;
+    jellyJump.repeatCount = INFINITY;
+    
+    jellyJump.timingFunction = [CAMediaTimingFunction functionWithControlPoints:0 :0 :0 :0];
+    
+    [self.layer addAnimation:jellyJump forKey:@"jellyJump"];
+}
+
+@end
--- JellyJump/JellyJump/ViewController.m
+++ JellyJump/JellyJump/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "UIView+JellyJump.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIView *jelly;
 
 @end
 
@@ -17,6 +20,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    UIView *tmp = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+//    tmp.backgroundColor = [UIColor redColor];
+//    
+//    [self.view addSubview:tmp];
+//    
+//    
+//    _jelly = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+//    _jelly.backgroundColor = [UIColor grayColor];
+//    [self.view addSubview:_jelly];
+    
+//    CGFloat sx = 1;
+//    CGFloat sy = 1;
+//    CGFloat sz = 1;
+//    
+//    
+//    CAKeyframeAnimation *jellyJump = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+//    
+//    NSValue *value0 = [NSValue valueWithCATransform3D:CATransform3DMakeScale(sx, sy, sz)];
+//    
+//    sx = 1.1;
+//    sy = 0.9;
+//    
+//    NSValue *value1 = [NSValue valueWithCATransform3D:CATransform3DMakeScale(sx, sy, sz)];
+//    CATransform3D scale1 = CATransform3DMakeScale(0.9, 1.1, 1);
+//    CATransform3D move1 = CATransform3DMakeTranslation(0, -50, 0);
+//    NSValue *value2 = [NSValue valueWithCATransform3D:CATransform3DConcat(scale1, move1)];
+//    CATransform3D scale2 = CATransform3DMakeScale(1, 1, 1);
+//    CATransform3D move2 = CATransform3DMakeTranslation(0, 0, 0);
+//    NSValue *value3 = [NSValue valueWithCATransform3D:CATransform3DConcat(scale2, move2)];
+//    
+//    
+//    jellyJump.values = @[value0, value1, value2, value3];
+//    
+//    jellyJump.duration = 1;
+//    jellyJump.fillMode = kCAFillModeBackwards;
+//    jellyJump.removedOnCompletion = NO;
+//    jellyJump.repeatCount = INFINITY;
+    
+//    [_jelly startJump];
+    
+    UIImageView *coin = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"image"]];
+    coin.frame = CGRectMake(100, 100, 100, 100);
+    [self.view addSubview:coin];
+    
+    [coin startJump];
+    
+    
 }
 
 
